c41cd0a8dba368c7a97c5b378664e81ff4657bca
TakuHazard <marwendot@gmail.com>
2020-08-22 16:21:17 -0700

    Created JavaFx project

---
+++ src/com/taku/view/First.java
@@ -0,0 +1,11 @@
+package com.taku.view;
+
+import javafx.fxml.FXML;
+
+public class First {
+
+    @FXML
+    public void buttonAction(){
+        System.out.println("CLICKED BUTTON");
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module JavaFxEmailClientCourse {
+
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.web;
+
+    opens com.taku;
+    opens com.taku.view;
+}
